62053e137cf10287fb636f5e9218e7bdd9a2344b
Xuan <97ssps30212@gmail.com>
2023-01-14 17:12:56 +0800

    feat: complete bob

---
+++ bob/src/lib.rs
@@ -0,0 +1,16 @@
+pub fn reply(message: &str) -> &str {
+    let message = message.trim();
+    if message.is_empty() {
+        return "Fine. Be that way!";
+    }
+    let is_question = message.ends_with("?");
+    let is_yelling =
+        message.chars().any(|c| c.is_alphabetic()) && message == message.to_uppercase();
+
+    match (is_question, is_yelling) {
+        (true, true) => "Calm down, I know what I'm doing!",
+        (true, false) => "Sure.",
+        (false, true) => "Whoa, chill out!",
+        (false, false) => "Whatever.",
+    }
+}
