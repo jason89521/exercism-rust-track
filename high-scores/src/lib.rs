e4d9df308bd4ca3b4a570b2a53e97261d5849d5f
Xuan <97ssps30212@gmail.com>
2023-01-15 21:13:13 +0800

    feat: complete high-scores

---
+++ high-scores/src/lib.rs
@@ -0,0 +1,29 @@
+#[derive(Debug)]
+pub struct HighScores<'a> {
+    scores: &'a [u32],
+}
+
+impl<'a> HighScores<'a> {
+    pub fn new(scores: &'a [u32]) -> Self {
+        Self { scores }
+    }
+
+    pub fn scores(&self) -> &[u32] {
+        &self.scores
+    }
+
+    pub fn latest(&self) -> Option<u32> {
+        self.scores.last().cloned()
+    }
+
+    pub fn personal_best(&self) -> Option<u32> {
+        self.scores.iter().max().cloned()
+    }
+
+    pub fn personal_top_three(&self) -> Vec<u32> {
+        let mut vec = self.scores.to_vec();
+        vec.sort_by(|a, b| b.cmp(a));
+        vec.truncate(3);
+        vec
+    }
+}
