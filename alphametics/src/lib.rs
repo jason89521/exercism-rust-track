799bfc7ca0088b7731557a001133ecee6a7c8176
Xuan <97ssps30212@gmail.com>
2023-01-21 00:02:01 +0800

    refactor(alphametics): use a better approach

--- alphametics/src/lib.rs
+++ alphametics/src/lib.rs
@@ -1,99 +1,69 @@
 use std::collections::{HashMap, HashSet};
 
 pub fn solve(input: &str) -> Option<HashMap<char, u8>> {
-    let numbers = parse(input);
-    let char_set = numbers.iter().flat_map(|s| s.chars()).collect();
-    let is_zero_map = get_is_zero_map(&numbers);
-
-    return recursive(&char_set, &numbers, &is_zero_map, &[]);
+    let values = parse(input);
+    let mut permutation = vec![0; values.len()];
+    return recursive(&mut permutation, &values, &(0..10).collect::<Vec<_>>());
 }
 
-/// Return an array whose last element is the sum, and the other elements are the operands.
-fn parse(input: &str) -> Vec<&str> {
-    let parsed: Vec<&str> = input.split("==").collect();
-    let sum = parsed[1].trim();
-    let mut operands: Vec<&str> = parsed[0].split("+").map(|op| op.trim()).collect();
-    operands.push(sum);
-    operands
-}
-
-/// If the value is `true` then the key shouldn't be a zero.
-fn get_is_zero_map(array: &[&str]) -> HashMap<char, bool> {
+fn parse(input: &str) -> Vec<(char, i64, bool)> {
     let mut map = HashMap::new();
-    for op in array.iter() {
-        let mut is_first = true;
-        for c in op.chars() {
-            if is_first {
-                map.insert(c, true);
-                is_first = false;
-            } else {
-                map.entry(c).or_insert(false);
-            }
+    let mut leadings = HashSet::new();
+    let mut prev_char = ' ';
+    // To total number is negative, such that we can examine the permutation
+    // by checking whether the sum is zero.
+    let mut weight = -1;
+    input.chars().rev().for_each(|c| {
+        if c.is_alphabetic() {
+            *map.entry(c).or_insert(0) += weight;
+            weight *= 10;
+            prev_char = c;
+        } else {
+            weight = 1;
+            leadings.insert(prev_char);
         }
-    }
-    map
+    });
+    leadings.insert(prev_char);
+    map.iter()
+        .map(|(&ch, &v)| (ch, v, leadings.contains(&ch)))
+        .collect()
 }
 
 fn recursive(
-    char_set: &HashSet<char>,
-    numbers: &[&str],
-    is_zero_map: &HashMap<char, bool>,
-    permutation: &[u8],
+    permutation: &mut [u8],
+    values: &[(char, i64, bool)],
+    unused_digits: &[u8],
 ) -> Option<HashMap<char, u8>> {
-    if char_set.len() == permutation.len() {
-        let solution: HashMap<char, u8> = char_set
-            .iter()
-            .cloned()
-            .zip(permutation.iter().cloned())
-            .collect();
-        if check(&solution, numbers, is_zero_map) {
+    // This index is shared by the `permutation` and `values`
+    let value_index = 10 - unused_digits.len();
+    if value_index == values.len() {
+        let mut sum = 0;
+        for (index, &digit) in permutation.iter().enumerate() {
+            sum += digit as i64 * values[index].1;
+        }
+        if sum == 0 {
+            let solution = permutation
+                .iter()
+                .zip(values.iter())
+                .map(|(&digit, &(ch, _, _))| (ch, digit))
+                .collect();
             return Some(solution);
         } else {
             return None;
         }
     }
+    for (index, &digit) in unused_digits.iter().enumerate() {
+        if digit == 0 && values[value_index].2 {
+            continue;
+        }
 
-    for i in 0..=9 {
-        if !permutation.contains(&i) {
-            if let Some(solution) = recursive(
-                char_set,
-                numbers,
-                is_zero_map,
-                &[permutation, &[i]].concat(),
-            ) {
-                return Some(solution);
-            }
+        permutation[value_index] = digit;
+        let mut unused_digits = unused_digits.to_vec();
+        unused_digits.remove(index);
+        if let Some(solution) = recursive(permutation, values, &unused_digits) {
+            return Some(solution);
         }
     }
 
     None
-}
-
-fn check(
-    solution: &HashMap<char, u8>,
-    numbers: &[&str],
-    is_zero_map: &HashMap<char, bool>,
-) -> bool {
-    if is_zero_map
-        .iter()
-        .filter(|&(_, &is_zero)| is_zero)
-        .any(|(c, _)| solution[c] == 0)
-    {
-        return false;
-    }
-
-    let numbers = numbers
-        .iter()
-        .map(|s| {
-            s.chars()
-                .map(|c| solution[&c].to_string())
-                .collect::<String>()
-        })
-        .map(|s| s.parse::<u64>().unwrap())
-        .collect::<Vec<_>>();
-    if numbers[0..numbers.len() - 1].iter().sum::<u64>() == numbers[numbers.len() - 1] {
-        return true;
-    } else {
-        return false;
-    }
 }
